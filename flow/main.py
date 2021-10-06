cc299ee13abce90dfdef2b47244594448c377502
vpcarlos <carlos.valdivia@orbitalads.com>
2021-10-06 23:29:58 +0200

    Add main workflow

--- flow/main.py
+++ flow/main.py
@@ -1,9 +1,49 @@
 import typer
-from flow.commands import feature
+
+from enum import Enum
+from flow.models.flow import Flow, Release, Feature
 
 app = typer.Typer()
-app.add_typer(feature.app, name='feature', help='Flow feature',)
+start = typer.Typer()
+
+app.add_typer(start, name='start')
+
+
+class VersionType(str, Enum):
+    major = 'major'
+    minor = 'minor'
+    patch = 'patch'
+
+
+@start.command()
+def feature(name: str):
+    """Start a new flow feature branch. """
+    Feature.git_flow('start', name)
+
+
+@start.command()
+def release(version_type: VersionType):
+    """Start a new flow release branch. """
+    next_v = Release.build_next(version_type.value)
+    Release.git_flow('start', next_v)
+
+
+@app.command()
+def publish():
+    """Publish flow. """
+    flow = Flow.factory()
+    branch = flow.get_flow_branch()
+    flow.git_flow('publish', branch)
+    typer.launch(flow.get_new_pr_url())
+
+
+@app.command()
+def finish():
+    """Finish flow. """
+    flow = Flow.factory()
+    branch = flow.get_flow_branch()
+    flow.git_flow('finish', branch)
 
 
 if __name__ == "__main__":
-    typer.run(app)
+    app()
--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,5 +1,1 @@
-from flow import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
+from typer.testing import CliRunner
